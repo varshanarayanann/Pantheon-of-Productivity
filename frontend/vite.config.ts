82dac7acda73cc01920d53de54171580fe935cfb
Varsha <varshanarayanan06@gmail.com>
2026-01-10 16:05:30 -0500

    vite change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    root: __dirname, // ✅ frontend is the root
+    root: '.', // ✅ frontend is the root
     server: {
       port: 3000,
       host: "0.0.0.0",
